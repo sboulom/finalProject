a57ebaae1aaa5358350452a21bff6d114626ba42
Abelard Bautista <abelardbautista@gmail.com>
2020-05-31 23:58:56 -0700

    Commented out code referencing unused stylesheets.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Home'
-import { NavigationBar } from './components/NavigationBar'
-
+import React from "react";
+// import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import { NavigationBar } from "./components/NavigationBar";
 
 function App() {
   return (
     <React.Fragment>
       <NavigationBar />
-        <Router>
-        <Route exact path ='/' component = {Home} />
-    
-    
-    
+      <Router>
+        <Route exact path="/" component={Home} />
       </Router>
     </React.Fragment>
   );
