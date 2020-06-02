99ed8effb1f184fd98578fbfccf2472f80e3c287
Steven Boulom <sboulom@gmail.com>
2020-06-02 14:17:15 -0700

    navigation bar issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
-import { NavigationBar } from './components/NavigationBar'
+import NavigationBar from './components/NavigationBar'
 
 
 function App() {
