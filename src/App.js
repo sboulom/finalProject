00debaa27bfe2104f2f0b02efa8b54c9a6dfb9e6
Nicholas Jaquez <nrjaquez@gmail.com>
2020-06-02 20:51:40 -0700

    added registration comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import { NavigationBar } from "./components/NavigationBar";
+import Wrapper from "./components/Wrapper/wrapper";
+import SignInPage from "./components/SignInPage/signIn";
+import Registration from "./components/Registration/registration";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <Router>
         <Route exact path="/" component={Home} />
       </Router>
+      <Wrapper>
+        <Registration />
+        <SignInPage/>
+      </Wrapper>
     </React.Fragment>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ export const Home = () => (
     <div>
         <h1> Hello Abelard, Robert and Nick</h1>
         <p>PLAIN TEXT</p>
+     
     </div>
+  
 )
 
 
