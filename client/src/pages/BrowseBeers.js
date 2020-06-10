8ccea9a507bda9d3615885c859d6db011902416b
Abelard Bautista <abelardbautista@gmail.com>
2020-06-09 17:50:17 -0700

    Restructured card elements.

--- client/src/pages/BrowseBeers.js
+++ client/src/pages/BrowseBeers.js
@@ -47,7 +47,7 @@ function BrowseBeers() {
       <Row>
         <Col>
           <div className="beerContainer">
-            {beers.data.slice(0, 5).map((beer, index) => {
+            {beers.data.slice(0, 2).map((beer, index) => {
               return (
                 <Card className="beerCard">
                   <Card.Img variant="top" src="" className="cardImage" />
@@ -57,12 +57,13 @@ function BrowseBeers() {
                     </h3>
                     {/* <Card.Title>Name of Beer</Card.Title> */}
                     <Card.Title>{beer.name}</Card.Title>
+                    <Card.Title style={{ fontSize: 13 }}>
+                      {beer.style.name} | {beer.abv}% ABV
+                    </Card.Title>
                     <Card.Text>
                       <ul>
-                        <li>Beer Style: {beer.style.name}</li>
-                        <li>ABV: {beer.abv}%</li>
-                        <li>Excerpt of Description: {}</li>
                         <li>Category: {beer.style.category.name}</li>
+                        <li>Excerpt of Description: {}</li>
                       </ul>
                     </Card.Text>
                   </Card.Body>
