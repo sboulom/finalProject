611143f333c9b9314b426bd032e7226382874297
Abelard Bautista <abelardbautista@gmail.com>
2020-06-11 19:35:23 -0700

    Resolved merge conflict with Mongo -> Cards.

--- client/src/pages/BrowseBeers.js
+++ client/src/pages/BrowseBeers.js
@@ -8,36 +8,22 @@ import Button from "../components/Button";
 import "./BrowseBeers.css";
 
 function BrowseBeers() {
-  const [beers, setBeers] = useState({ data: [] });
+  // For API -> Card Usage:
+  // const [beers, setBeers] = useState({ data: [] });
+  const [beers, setBeers] = useState([]);
   const [beerSearch, setBeerSearch] = useState("");
-  // const [Description, setDescription] = useState();
-  // const [StyleDescription, setStyleDescription] = useState();
 
-  useEffect(() => {
-    // DB.getBeers()
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-    API.getBeers()
-      .then((res) => {
-        setBeers(res.data);
-        // console.log(res.data.data[0]);
-        // res.data.data.slice(0, 2).map((beer, index) => {
-        //   if (beer.description == null) {
-        //     return beer.style.description;
-        //   } else {
-        //     return beer.description;
-        //   }
-        // });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    console.log("useEffect has been called");
-  }, []);
+  // For API -> Card Usage:
+  // useEffect(() => {
+  //   API.getBeers()
+  //     .then((res) => {
+  //       setBeers(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  //   console.log("useEffect has been called");
+  // }, []);
 
   const handleInputChange = (event) => {
     // Destructure the name and value properties off of event.target
@@ -55,16 +41,7 @@ function BrowseBeers() {
       .catch((err) => console.log(err));
   };
 
-  // DB.getBeers(beerSearch)
-  //   .then(res => setBeers(res.data))
-  //   .catch(err => console.log(err));
-
   return (
-    // <Container className="browseBeer">
-    //   <h1>Browse Beer</h1>
-    //   <Row>
-    //     <Col>
-    //       <div className="beerContainer">
     <Container className="browseBeer">
       <h1>Browse Beer</h1>
       <Row>
@@ -86,38 +63,44 @@ function BrowseBeers() {
           </form>
         </Col>
       </Row>
-      <Row>
-        {beers.data.slice(0, 12).map((beer, index) => {
-          return (
-            <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
-              <Card className="cardHeight">
-                <Card className="cardHeight cardColor">
-                  <Card.Title className="ml-auto">
-                    <h4>
-                      <i className="fas fa-plus-circle"></i>
-                    </h4>
-                  </Card.Title>
-                  <Card.Img
-                    variant="top"
-                    src="./assets/pixelMug.jpg"
-                    className="cardImage mx-auto"
-                  />
-                  <Card.Body>
-                    <Card.Title className="text-center">{beer.name}</Card.Title>
-                    <Card.Title className="subText">
-                      {beer.style.name} | {beer.abv}% ABV
+      {!beers.length ? (
+        <h1 className="text-center">No Beers to Display</h1>
+      ) : (
+        <Row>
+          {beers.slice(0, 12).map((beer, index) => {
+            return (
+              <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
+                <Card className="cardHeight">
+                  <Card className="cardHeight cardColor">
+                    <Card.Title className="ml-auto">
+                      <h4>
+                        <i className="fas fa-plus-circle"></i>
+                      </h4>
                     </Card.Title>
-                    <Card.Text className="subText">
-                      {beer.style.category.name}
-                    </Card.Text>
-                    <Card.Text>Description: {}</Card.Text>
-                  </Card.Body>
+                    <Card.Img
+                      variant="top"
+                      src="./assets/pixelMug.jpg"
+                      className="cardImage mx-auto"
+                    />
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        {beer.name}
+                      </Card.Title>
+                      <Card.Title className="subText">
+                        {beer.beerStyle} | {beer.abv}% ABV
+                      </Card.Title>
+                      <Card.Text className="subText">
+                        {beer.beerCategory}
+                      </Card.Text>
+                      <Card.Text>Description: {}</Card.Text>
+                    </Card.Body>
+                  </Card>
                 </Card>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </Container>
   );
 }
