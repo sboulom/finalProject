4a34011f6d79008257ddb26b0d9e3ea6cd03d048
Nicholas Jaquez <nrjaquez@gmail.com>
2020-06-13 17:50:16 -0700

    add a beer form at center of page

--- client/src/pages/AddABeer.js
+++ client/src/pages/AddABeer.js
@@ -1,112 +1,99 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import React from "react";
+// originally tried "useState" on the top
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import "../pages/AddABeer.css";
 
-
 const AddABeer = () => {
-
-  const [beers, setBeers] = useState({ data: [] });
+  // const [beers, setBeers] = useState({ data: [] });
 
   return (
-<div>
-<Container>
-      <Row>
-        <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
-          <Card className="cardHeight">
-            <Card className="cardHeight cardColor">
-              <Card.Title className="ml-auto">
-                <h4>
-                  <i className="fas fa-plus-circle"></i>
-                </h4>
-              </Card.Title>
-              <Card.Img
-                variant="top"
-                src="./assets/pixelMug.jpg"
-                className="cardImage mx-auto"
-              />
-              <Card.Body>
-                <Card.Title className="text-center">Name of Beer</Card.Title>
-                <Card.Title className="subText">
-                  Style of Beer | A number goes here lmao% ABV
+    <div >
+      <Container >
+        <Row>
+          <Col xs={6} sm={4} md={3} lg={8} xl={8} className="my-1 px-0" className="addBeer" >
+            <Card className="cardHeight">
+              <Card className="cardHeight cardColor">
+                <Card.Title className="ml-auto">
+                  <h4>
+                    <i className="fas fa-plus-circle"></i>
+                  </h4>
                 </Card.Title>
-                <Card.Text className="subText">Category of Beer</Card.Text>
-                <Card.Text>Description: {}</Card.Text>
-              </Card.Body>
+                <Card.Img
+                  variant="top"
+                  src="./assets/pixelMug.jpg"
+                  className="cardImage mx-auto"
+                />
+                <Card.Body>
+                  <Card.Title className="text-center">Name of Beer</Card.Title>
+                  <Card.Title className="subText">
+                    <Form>
+                      <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Name of Beer</Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter Beer Name Here"
+                        />
+                      </Form.Group>
+                    </Form>
+                    <Container>
+                      <Row>
+                        <Col>
+                          <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                              <Form.Label>Style of Beer</Form.Label>
+                              <Form.Control
+                                type="email"
+                                placeholder="Enter Beer Style Here"
+                              />
+                            </Form.Group>
+                          </Form>
+                        </Col>
+                        <Col>
+                          <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                              <Form.Label>ABV</Form.Label>
+                              <Form.Control
+                                type="email"
+                                placeholder="Enter ABV Here"
+                              />
+                            </Form.Group>
+                          </Form>
+                        </Col>
+                      </Row>
+                    </Container>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                      <Form.Label>Category of Beer</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="Enter Category of Beer Here"
+                      />
+                    </Form.Group>
+                  </Card.Title>
+                  <Card.Text className="subText"></Card.Text>
+                  {/* <Card.Text>Description: {}</Card.Text> */}
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Beer Description</Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                  </Form.Group>
+                  <div >
+                  <Container >
+                    <Row >
+                      <Col className="button">
+                      <Button variant="success" >Add Beer</Button>{" "}
+
+                      </Col>
+                    </Row>
+                  </Container>
+                  </div>
+                </Card.Body>
+              </Card>
             </Card>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-
-    <div className="addBeerForm">
-      <div className="beerName">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Beer Name"
-          aria-label="Recipient's username"
-          aria-describedby="button-addon2"
-        />
-        <div class="input-group-append">
-
-        </div>
-      </div>
-
-      <div className="beerName">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Style of Beer"
-          aria-label="Recipient's username"
-          aria-describedby="button-addon2"
-        />
-        <div class="input-group-append">
-
-        </div>
-      </div>
-      <div className="beerName">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="ABV"
-          aria-label="Recipient's username"
-          aria-describedby="button-addon2"
-        />
-        <div class="input-group-append">
-
-        </div>
-      </div>
-      <div className="beerName">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Beer Category"
-          aria-label="Recipient's username"
-          aria-describedby="button-addon2"
-        />
-        <div class="input-group-append">
-
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="exampleFormControlTextarea1"></label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          placeholder="Short Description"
-          rows="3"
-        ></textarea>
-              <button
-            className="descriptionButton"
-            type="button"
-            id="button-addon2"
-          >
-            Beer Me
-          </button>
-      </div>
-    </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
 
 export default AddABeer;
+
