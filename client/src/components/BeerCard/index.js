52b800b574d12d70cd99990b5f8126246f1145ed
Abelard Bautista <abelardbautista@gmail.com>
2020-06-16 01:50:26 -0700

    Added blank h4 element for future development/card image spacing.

--- client/src/components/BeerCard/index.js
+++ client/src/components/BeerCard/index.js
@@ -6,6 +6,11 @@ function BeerCard(beer) {
     <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
       <Card className="cardHeight">
         <Card className="cardHeight cardColor">
+          <Card.Title className="ml-auto">
+            <h4>
+              <i className="mx-1" />
+            </h4>
+          </Card.Title>
           <Card.Img
             variant="top"
             src="./assets/pixelMug.jpg"
