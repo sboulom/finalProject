7c1b7e0220a440565d0c49d361f0d2a5224c17d2
Abelard Bautista <abelardbautista@gmail.com>
2020-06-16 00:31:18 -0700

    Implemented beerIcon into NavBar.

--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -4,18 +4,36 @@ import { Nav, Navbar } from "react-bootstrap";
 import "./index.css";
 
 export const NavigationBar = () => (
-  <div className='navbar navbar-expand'>
-    <Navbar className= 'innerNavbar' bg="light" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" />
-        <Nav.Link href="/"><i class="fas fa-home"> Home</i></Nav.Link>
-        <Nav.Link href='/'><i class="fas fa-house-user"> Profile</i></Nav.Link>
-        <Nav.Link href='/Browse'><i class="fas fa-beer"> Browse Beers</i></Nav.Link>
-        <Nav.Link href='/addabeer'><i class="fas fa-plus-square"> Add A Beer</i></Nav.Link>
-        <Nav.Link href='/signin'>Sign In</Nav.Link>
-        <Nav.Link href='/register'>Register</Nav.Link>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
+  // <div className="navbar navbar-expand">
+  <Navbar className="innerNavbar" bg="light" expand="lg">
+    <Navbar.Brand className="mx-1">
+      <img
+        src="./assets/beerIcon.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="beerIcon logo"
+      />
+    </Navbar.Brand>
+    <Navbar.Brand>TeamBeer</Navbar.Brand>
+    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+    {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+    <Nav className="mr-auto" />
+    <Nav.Link href="/">
+      <i class="fas fa-home"> Home</i>
+    </Nav.Link>
+    <Nav.Link href="/">
+      <i class="fas fa-house-user"> Profile</i>
+    </Nav.Link>
+    <Nav.Link href="/Browse">
+      <i class="fas fa-beer"> Browse Beers</i>
+    </Nav.Link>
+    <Nav.Link href="/addabeer">
+      <i class="fas fa-plus-square"> Add A Beer</i>
+    </Nav.Link>
+    <Nav.Link href="/signin">Sign In</Nav.Link>
+    <Nav.Link href="/register">Register</Nav.Link>
+    {/* </Navbar.Collapse> */}
+  </Navbar>
+  // </div>
 );
