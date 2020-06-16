ea328e0dca05be7176592040ea758d0e4c428b0c
Abelard Bautista <abelardbautista@gmail.com>
2020-06-15 22:36:10 -0700

    Search prototype created.

--- client/src/pages/BrowseBeers.js
+++ client/src/pages/BrowseBeers.js
@@ -9,36 +9,39 @@ import "./BrowseBeers.css";
 function BrowseBeers() {
   // For API -> Card Usage:
   const [beers, setBeers] = useState({ data: [] });
-  // const [beers, setBeers] = useState([]);
-  const [beerSearch, setBeerSearch] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   // For API -> Card Usage:
   useEffect(() => {
-    API.getBeers()
-      .then((res) => {
-        setBeers(res.data);
-        // console.log(res.data[0]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    API.getBeers().then((res) => {
+      setBeers(res.data);
+    });
     console.log("useEffect has been called");
   }, []);
+
+  useEffect(() => {
+    const results = beers.data
+      .slice(0, 6)
+      .filter((beer) => beer.name.toLowerCase().includes(searchTerm));
+    setSearchResults(results);
+  }, [searchTerm]);
 
   // Destructure the name and value properties off of event.target
   // Update the appropriate state
   const handleInputChange = (event) => {
     const { value } = event.target;
-    setBeerSearch(value);
+    setSearchTerm(value);
   };
 
   // When the form is submitted, prevent its default behavior
   //  Get BeersLocal update the BeersLocal state
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log(beerSearch);
-    API.getBeersLocal(beerSearch)
-      .then((res) => setBeers(res.data))
+    console.log(searchTerm);
+
+    API.getBeersLocal(searchTerm)
+      .then((res) => setSearchResults(res.data))
       .catch((err) => console.log(err));
   };
 
@@ -53,8 +56,8 @@ function BrowseBeers() {
             <Row>
               <Col xs={12} sm={12} md={10} lg={10} xl={10}>
                 <Input
-                  name="BeerSearch"
-                  value={beerSearch}
+                  name="SearchTerm"
+                  value={searchTerm}
                   onChange={handleInputChange}
                   placeholder="Search For a Beer"
                 />
@@ -72,41 +75,79 @@ function BrowseBeers() {
           </form>
         </Col>
       </Row>
-      {/* {!beers.length ? (
-        <h1 className="text-center">No Beers to Display</h1>
-      ) : ( */}
-      <Row>
-        {beers.data.slice(0, 6).map((beer, index) => {
-          return (
-            <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
-              <Card className="cardHeight">
-                <Card className="cardHeight cardColor">
-                  <Card.Title className="ml-auto">
-                    <h4>
-                      <i className="fas fa-plus-circle"></i>
-                    </h4>
-                  </Card.Title>
-                  <Card.Img
-                    variant="top"
-                    src="./assets/pixelMug.jpg"
-                    className="cardImage mx-auto"
-                  />
-                  <Card.Body>
-                    <Card.Title className="text-center">{beer.name}</Card.Title>
-                    <Card.Title className="subText">
-                      {beer.style.name} | {beer.abv}% ABV
+      {searchResults.length ? (
+        <Row>
+          {/* Search: All Beers filtered by Search */}
+          {searchResults.map((beer, index) => {
+            return (
+              <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
+                <Card className="cardHeight">
+                  <Card className="cardHeight cardColor">
+                    <Card.Title className="ml-auto">
+                      <h4>
+                        <i className="fas fa-plus-circle"></i>
+                      </h4>
                     </Card.Title>
-                    <Card.Text className="subText">
-                      {beer.style.category.name}
-                    </Card.Text>
-                    <Card.Text>Description: {}</Card.Text>
-                  </Card.Body>
+                    <Card.Img
+                      variant="top"
+                      src="./assets/pixelMug.jpg"
+                      className="cardImage mx-auto"
+                    />
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        {beer.name}
+                      </Card.Title>
+                      <Card.Title className="subText">
+                        {beer.style.name} | {beer.abv}% ABV
+                      </Card.Title>
+                      <Card.Text className="subText">
+                        {beer.style.category.name}
+                      </Card.Text>
+                      <Card.Text>Description: {}</Card.Text>
+                    </Card.Body>
+                  </Card>
                 </Card>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
+              </Col>
+            );
+          })}
+        </Row>
+      ) : (
+        <Row>
+          {/* Default: All Beers from API */}
+          {beers.data.slice(0, 6).map((beer, index) => {
+            return (
+              <Col xs={6} sm={4} md={3} lg={2} xl={2} className="my-1 px-0">
+                <Card className="cardHeight">
+                  <Card className="cardHeight cardColor">
+                    <Card.Title className="ml-auto">
+                      <h4>
+                        <i className="fas fa-plus-circle"></i>
+                      </h4>
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src="./assets/pixelMug.jpg"
+                      className="cardImage mx-auto"
+                    />
+                    <Card.Body>
+                      <Card.Title className="text-center">
+                        {beer.name}
+                      </Card.Title>
+                      <Card.Title className="subText">
+                        {beer.style.name} | {beer.abv}% ABV
+                      </Card.Title>
+                      <Card.Text className="subText">
+                        {beer.style.category.name}
+                      </Card.Text>
+                      <Card.Text>Description: {}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </Container>
   );
 }
