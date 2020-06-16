644ff1fc4e9af2cf1175bbb2660ae2f4a598f4c7
Abelard Bautista <abelardbautista@gmail.com>
2020-06-16 03:40:09 -0700

    Adjusted mobile breakpoints.

--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./index.css";
 
 export const NavigationBar = () => (
-  <Navbar className="innerNavbar" bg="light" expand="lg">
+  <Navbar collapseOnSelect className="navPaddingMobile" bg="light" expand="lg">
     <Navbar.Brand className="mx-1">
       <img
         src="./assets/beerIcon.png"
@@ -13,7 +13,7 @@ export const NavigationBar = () => (
         alt="beerIcon logo"
       />
     </Navbar.Brand>
-    <Navbar.Brand className="">TeamBeer</Navbar.Brand>
+    <Navbar.Brand>TeamBeer</Navbar.Brand>
     <Nav.Link href="/" className="navIconPadding">
       <i className="fas fa-house-user mx-1"></i>
       <Navbar.Text className="navTextMobile">User Profile</Navbar.Text>
@@ -22,15 +22,20 @@ export const NavigationBar = () => (
       <i className="fas fa-beer mx-1"></i>
       <Navbar.Text className="navTextMobile">Browse Beers</Navbar.Text>
     </Nav.Link>
-    <Nav.Link href="/addabeer" className="mr-auto navIconPadding">
+    <Nav.Link href="/addabeer" className="navIconPadding">
       <i className="fas fa-plus-circle mx-1"></i>
       <Navbar.Text className="navTextMobile">Add Custom Beer</Navbar.Text>
     </Nav.Link>
-    <Nav.Link href="/signin">
-      <Navbar.Text>Sign In</Navbar.Text>
-    </Nav.Link>
-    <Nav.Link href="/register">
-      <Navbar.Text>Register</Navbar.Text>
-    </Nav.Link>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link href="/signin">
+          <Navbar.Text>Sign In</Navbar.Text>
+        </Nav.Link>
+        <Nav.Link href="/register">
+          <Navbar.Text>Register</Navbar.Text>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
