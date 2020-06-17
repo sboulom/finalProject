e60abc3048bea80b13c8b5df185f1aa4f074c2f4
Steven Boulom <sboulom@gmail.com>
2020-06-16 20:15:32 -0700

    added log out feature

--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -1,63 +1,78 @@
-import React, { useState, useEffect } from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import "./index.css";
-import API from "../../utils/API"
+import React, {useState, useEffect} from 'react'
+import {Nav, Navbar} from 'react-bootstrap'
+import './index.css'
+import API from '../../utils/API'
 
 export const NavigationBar = () => {
-    const [state, setState]= useState({
-    name: ""
-  }) 
-  useEffect(()=>{
-    API.getCurrentUserData().then((user)=> {
-      console.log("user", user)
+  const [state, setState] = useState({
+    name: '',
+  })
+  useEffect(() => {
+    API.getCurrentUserData().then((user) => {
+      console.log('user', user)
       setState({
-        name: user.data.name
+        name: user.data.name,
       })
     })
-    },[])
-    const logOut = () => {
-      API.logOut().then(()=>{
-        window.location.href="/"
-      })
-    }
-    return(
-      <div>
-  <Navbar collapseOnSelect className="navPaddingMobile" bg="light" expand="lg">
-    <Navbar.Brand className="mx-1">
-      <img
-        src="./assets/beerIcon.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="beerIcon logo"
-      />
-    </Navbar.Brand>
-    <Navbar.Brand>TeamBeer</Navbar.Brand>
-    <Nav.Link href="/" className="navIconPadding">
-      <i className="fas fa-house-user mx-1"></i>
-      <Navbar.Text className="navTextMobile">User Profile</Navbar.Text>
-    </Nav.Link>
-    <Nav.Link href="/Browse" className="navIconPadding">
-      <i className="fas fa-beer mx-1"></i>
-      <Navbar.Text className="navTextMobile">Browse Beers</Navbar.Text>
-    </Nav.Link>
-    <Nav.Link href="/addabeer" className="navIconPadding">
-      <i className="fas fa-plus-circle mx-1"></i>
-      <Navbar.Text className="navTextMobile">Add Custom Beer</Navbar.Text>
-    </Nav.Link>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        <Nav.Link href="/signin">
-          <Navbar.Text>Sign In</Navbar.Text>
-        </Nav.Link>
-        <Nav.Link href="/register">
-          <Navbar.Text>Register</Navbar.Text>
-        </Nav.Link>
-      </Nav>
-
-    </Navbar.Collapse>
-  </Navbar>
-  </div>
-  )
+  }, [])
+  const logOut = () => {
+    API.logOut().then(() => {
+      window.location.href = '/'
+    })
   }
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        className='navPaddingMobile'
+        bg='light'
+        expand='lg'
+      >
+        <Navbar.Brand className='mx-1'>
+          <img
+            src='./assets/beerIcon.png'
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+            alt='beerIcon logo'
+          />
+        </Navbar.Brand>
+        <Navbar.Brand>TeamBeer</Navbar.Brand>
+        <Nav.Link href='/' className='navIconPadding'>
+          <i className='fas fa-house-user mx-1'></i>
+          <Navbar.Text className='navTextMobile'>User Profile</Navbar.Text>
+        </Nav.Link>
+        <Nav.Link href='/Browse' className='navIconPadding'>
+          <i className='fas fa-beer mx-1'></i>
+          <Navbar.Text className='navTextMobile'>Browse Beers</Navbar.Text>
+        </Nav.Link>
+        <Nav.Link href='/addabeer' className='navIconPadding'>
+          <i className='fas fa-plus-circle mx-1'></i>
+          <Navbar.Text className='navTextMobile'>Add Custom Beer</Navbar.Text>
+        </Nav.Link>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <div className= "ml-auto">
+          {state.name ? (
+            <div>
+              <h3>Welcome {state.name}</h3>
+              <Nav.Link href='#' onClick={logOut}>
+                Log Out
+              </Nav.Link>
+            </div>
+          ) : (
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ml-auto'>
+                <Nav.Link href='/signin'>
+                  <Navbar.Text>Sign In</Navbar.Text>
+                </Nav.Link>
+                <Nav.Link href='/register'>
+                  <Navbar.Text>Register</Navbar.Text>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          )}
+        </div>
+      </Navbar>
+    </div>
+  )
+}
