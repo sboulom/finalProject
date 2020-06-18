6a3db80202c9903dbf14e14609f2952ef3ed8e2d
Abelard Bautista <abelardbautista@gmail.com>
2020-06-17 19:17:57 -0700

    Test Heroku deployment: rerouted static file path.

--- server.js
+++ server.js
@@ -27,7 +27,15 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static("client/build"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 // Connect to the Mongo DB
