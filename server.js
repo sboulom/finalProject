10e19e423d309e499ea1e4265b270e34d793ca0e
Donte´ Burney <digitalartcrew@gmail.com>
2020-06-23 19:23:40 -1000

    Updated installation

--- server.js
+++ server.js
@@ -44,12 +44,12 @@ app.use("/api", apiRoutes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 } else {
   app.use(express.static(path.join(__dirname, "/client/public")));
-  app.get("/", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 }
